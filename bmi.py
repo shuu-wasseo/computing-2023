f9f093cb338a1d1cc98de7cc4fa5029cd467df37
shua <86475694+shuu-wasseo@users.noreply.github.com>
2023-01-13 11:22:22 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,4 +1,13 @@
 weight = float(input("weight in kg: "))
 height = float(input("height in m : "))
 bmi = weight/(height**2)
-print(round(bmi, 2))
+msg = (f"your bmi is {round(bmi, 2)}. ")
+if bmi < 18.5:
+    msg += "you are at risk of nutritional deficiency and osteoporosis."
+elif bmi < 22.9:
+    msg += "you are low-risk."
+elif bmi < 27.4:
+    msg += "you are moderate-risk."
+else:
+    msg += "you are high-risk."
+print(msg)
