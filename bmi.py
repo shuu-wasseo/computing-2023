836edbc3b20964b40b1c226ba93338ff9a6f246d
shua <86475694+shuu-wasseo@users.noreply.github.com>
2023-01-13 11:16:57 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,4 +1,4 @@
-weight = float(input("weight: "))
-height = float(input("height: "))
+weight = float(input("weight in kg: "))
+height = float(input("height in m : "))
 bmi = weight/(height**2)
 print(round(bmi, 2))
